08850e515249fb50e3311d9a9635e232ae373918
langdonglei <langdonglei@icloud.com>
2022-04-11 15:25:45 +0800

    fix

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
-echo 'xxjpq-php2';
+$token_path = '/.tencentcloudbase/wx/cloudbase_access_token';
+if (file_exists($token_path)) {
+    echo file_get_contents($token_path);
+    return 0;
+}
+echo 'no token file';
