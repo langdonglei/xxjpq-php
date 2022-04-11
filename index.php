1842e5113f68234a53176ec5d158c0941a206bf5
langdonglei <langdonglei@icloud.com>
2022-04-11 15:38:03 +0800

    fix

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php
-$token_path = '/.tencentcloudbase/wx/cloudbase_access_token';
-if (file_exists($token_path)) {
-    echo file_get_contents($token_path);
-    return 0;
+
+use GuzzleHttp\Client;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+/**
+ * @throws Exception
+ */
+function token()
+{
+    $token_path = '/.tencentcloudbase/wx/cloudbase_access_token';
+    if (file_exists($token_path)) {
+        return file_get_contents($token_path);
+    }
+    throw new Exception('no token file');
 }
-echo 'no token file';
+
+$token = token();
+echo $token . '<br><pre>';
+
+
+$client = new Client();
+$api = "https://api.weixin.qq.com/wxa/msg_sec_check?cloudbase_access_token=$token";
+$res = $client->post($api, [
+    'json' => [
+        'content' => 'abcd'
+    ]
+])->getBody()->getContents();
+$res = json_decode($res, true);
+print_r($res);
+
+
