10cea0ea3738dbedd52aac2513e0d27a8dcc75e3
langdonglei <langdonglei@icloud.com>
2022-04-12 10:08:32 +0800

    目录结构调整

--- public/index.php
+++ public/index.php
@@ -16,13 +16,12 @@ function token()
     throw new Exception('no token file');
 }
 
-// $token = token();
-// echo $token . '<br><pre>';
+ $token = token();
+ echo $token . '<br><pre>';
 
 
 $client = new Client();
-#$api = "https://api.weixin.qq.com/wxa/msg_sec_check?cloudbase_access_token=$token";
-$api = "http://api.weixin.qq.com/wxa/msg_sec_check?cloudbase_access_token=CL0BEoACRgKBrJ4pH9_fREbM59k5fYdvvC-gDXXLd3xg_Q_MWKqHMMl_1_brg7iurV7rdbby1L6gg0EJFzZPYNFff_lHa41a_aP0zjNFuxRNPbA1yqgrtmiTd81e3OXlxl9C1BLafkX2rSF5BFGimp6lcCMsMWTkACXLvEBetBPIEH_kpS67VA8Z2l7UxNlYYsN0NZNeQqf2s6oARVuY7aerbPtnlJd-HfNpthJzVXBegliInfUMXH72tA9hOBILDCTYsdaz8hpxvpD0vhXPD6CNrM3VkmOAkQ1jgMsWYnlWU3BHjrTDWzAwiAlfmlc4aIUTv6EK8DBraxY9-HAexGgAPNg9UhgFIAA";
+$api = "https://api.weixin.qq.com/wxa/msg_sec_check?cloudbase_access_token=$token";
 $res = $client->post($api, [
     'json' => [
         'content' => 'abcd'
