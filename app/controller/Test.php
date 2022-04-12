fc577316487aab140a6009945aacff0479793432
langdonglei <langdonglei@icloud.com>
2022-04-12 15:13:54 +0800

    删除不需要的目录

---
+++ app/controller/Test.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\controller;
+
+use Exception;
+use GuzzleHttp\Client;
+
+class Test
+{
+    public function index()
+    {
+        throw new Exception('test');
+        $token_path = '/.tencentcloudbase/wx/cloudbase_access_token';
+        if (file_exists($token_path)) {
+            $token = file_get_contents($token_path);
+            dump($token);
+        } else {
+            throw new Exception('no token file');
+        }
+
+        $client = new Client();
+        $api = "https://api.weixin.qq.com/wxa/msg_sec_check?cloudbase_access_token=$token";
+        $res = $client->post($api, [
+            'json' => [
+                'content' => 'abcd'
+            ]
+        ])->getBody()->getContents();
+        $res = json_decode($res, true);
+        dump($res);
+    }
+}
