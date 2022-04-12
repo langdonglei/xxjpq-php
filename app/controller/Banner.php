54e935e6fcc3d4cfdb80b34b0805ff134af75ed7
langdonglei <langdonglei@icloud.com>
2022-04-12 23:17:41 +0800

    api banner

--- app/controller/Banner.php
+++ app/controller/Banner.php
@@ -9,8 +9,8 @@ class Banner
     public function index(): Json
     {
         return json([
-            'https://t7.baidu.com/it/u=4198287529,2774471735&fm=193&f=GIF',
-            'https://t7.baidu.com/it/u=1956604245,3662848045&fm=193&f=GIF'
+            ['imgUrl' => 'https://t7.baidu.com/it/u=4198287529,2774471735&fm=193&f=GIF', 'relation' => ''],
+            ['imgUrl' => 'https://t7.baidu.com/it/u=1956604245,3662848045&fm=193&f=GIF', 'relation' => '']
         ]);
     }
 }
