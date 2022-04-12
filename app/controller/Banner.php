07cc9361c78dcdb2c63597f4033aaa941fcff39a
langdonglei <langdonglei@icloud.com>
2022-04-12 22:54:50 +0800

    api banner

---
+++ app/controller/Banner.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\controller;
+
+use think\response\Json;
+
+class Banner
+{
+    public function index(): Json
+    {
+        return json([
+            'https://t7.baidu.com/it/u=4198287529,2774471735&fm=193&f=GIF',
+            'https://t7.baidu.com/it/u=1956604245,3662848045&fm=193&f=GIF'
+        ]);
+    }
+}
