0ca8902abb016afefb8d26b2f9304953dbcfa7df
langdonglei <langdonglei@icloud.com>
2022-04-12 23:21:48 +0800

    banner

--- app/controller/Banner.php
+++ app/controller/Banner.php
@@ -10,7 +10,7 @@ class Banner
     {
         return json([
             ['imgUrl' => 'https://t7.baidu.com/it/u=4198287529,2774471735&fm=193&f=GIF', 'relation' => ''],
-            ['imgUrl' => 'https://t7.baidu.com/it/u=1956604245,3662848045&fm=193&f=GIF', 'relation' => '']
+            ['imgUrl' => 'https://t7.baidu.com/it/u=1956604245,3662848045&fm=193&f=GIF', 'relation' => ''],
         ]);
     }
 }
